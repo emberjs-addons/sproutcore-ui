64077e7bb1efb4f48445d968ddfb9938340dc674
Majd Taby <mtaby@me.com>
2011-06-22 14:28:13 -0700

    Refactor tests

--- tests/acceptance_tests/navigation.js
+++ tests/acceptance_tests/navigation.js
@@ -10,65 +10,67 @@ var set = SC.set;
 var get = SC.get;
 
 NavigationAcceptanceTest = {};
-
-var navigationView;
-
-var navigation_tree = [
-  {
-    value: 'one',
-    children: [
-      { value: "one one" },
-      { value: "one two" },
-      { value: "one three" },
-      { value: "one four" },
-      { value: "one five" }
-    ]
-  },
-  {
-    value: 'two',
-    children: [
-      { value: "two one" },
-      { value: "two two" },
-      { value: "two three" },
-      { value: "two four" },
-      { value: "two five" }
-    ]
-  },
-  {
-    value: 'three',
-    children: [
-      { value: "three one" },
-      { value: "three two" },
-      { value: "three three" },
-      { value: "three four" },
-      { value: "three five" }
-    ]
-  }
-];
+var containerView;
 
 module("Navigation Acceptance test", {
   setup: function() {
 
-    NavigationAcceptanceTest.navigationController = UI.NavigationController.create({
-      tree: navigation_tree
-    });
+    SC.run(function() {
+      NavigationAcceptanceTest.navigationController = UI.NavigationController.create({
+        tree: [
+          {
+            value: 'one',
+            children: [
+              { value: "one one" },
+              { value: "one two" },
+              { value: "one three" },
+              { value: "one four" },
+              { value: "one five" }
+            ]
+          },
+          {
+            value: 'two',
+            children: [
+              { value: "two one" },
+              { value: "two two" },
+              { value: "two three" },
+              { value: "two four" },
+              { value: "two five" }
+            ]
+          },
+          {
+            value: 'three',
+            children: [
+              { value: "three one" },
+              { value: "three two" },
+              { value: "three three" },
+              { value: "three four" },
+              { value: "three five" }
+            ]
+          }
+        ]
+      });
 
-    navigationView = SC.View.create({
-      template: SC.Handlebars.compile('{{ui NavigationView elementId="navigation_view" controller="NavigationAcceptanceTest.navigationController"}}')
-    });
+      containerView = SC.View.create({
+        template: SC.Handlebars.compile('{{ui NavigationView elementId="navigation_view" controller="NavigationAcceptanceTest.navigationController"}}')
+      });
 
-    navigationView.append();
+      console.group('TEST: appending containerView');
+      containerView.append();
+      debugger;
+    });
   },
 
   teardown: function() {
-
+    console.groupEnd();
+    containerView.destroy();
   }  
 });
 
 test("#navigation_view should be a valid selector in DOM", function() {
-  equals($('#navigation_view').length,1);
+  equals(containerView.$('#navigation_view').length,1);
 });
 
 test("It should create a #navigation-item for each item", function() {
-  equals($('.ui-navigation-item').length,navigation_tree.length);
+  equals(containerView.$('.ui-navigation-item').length,navigation_tree.length);
 });
