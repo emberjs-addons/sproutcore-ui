4238aa9f371544224152cb11e8ecad47c3573a14
Majd Taby <mtaby@me.com>
2011-06-22 10:54:10 -0700

    Add handlebars helper

---
+++ tests/system/helper.js
@@ -0,0 +1,13 @@
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2011 Strobe Inc. and contributors.
+//            ©2008-2011 Apple Inc. All rights reserved.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
+module("Helper");
+
+test("{{ui}} helper must be registered", function() {
+  ok(Handlebars.helpers.ui);
+});
+
