499c054d4143ed13bc410f08cec1a66e113ee949
Majd Taby <mtaby@me.com>
2011-06-23 00:25:22 -0700

    Add basic template theming support (draft version)

---
+++ documentation/theming.md
@@ -0,0 +1,22 @@
+
+Theming
+===
+
+Overview
+---
+
+Theming is used to customize the look and feel of SproutCore UI and add
+branding to an application.
+
+Themes can override either the styling of the bundled controls, or
+override how they're rendering completely. 
+
+Usage
+---
+
+A theme consists of two components:
+
+1. A `my-theme.scss` file which defines CSS rules of the theme
+2. A collection of `component.handlebars` which form the templates used
+   to render the various components
+
---
+++ tests/views/view_tests.js
@@ -0,0 +1,56 @@
+// ==========================================================================
+// Project:  SproutCore Runtime
+// Copyright: ©2011 Strobe Inc. and contributors.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
+var templates = null;
+
+var get = SC.get;
+var set = SC.set;
+
+module("View Basics");
+
+test("SC.View has a themeName property (null value means it was defined)", function() {
+  equals(SC.View.create().themeName,null);
+});
+
+module("Theme Template support", {
+  setup: function() {
+    __templates_cache = SC.TEMPLATES;
+
+    SC.TEMPLATES = {
+      "c": "cTemplate"
+    };
+
+    templates = UI._Templates.create({
+      themeTemplates: {
+        "a": "aTemplate",
+        "b": "bTemplate"
+      }
+    });
+  },
+
+  teardown: function() {
+    templates.destroy();
+    SC.TEMPLATES = __templates_cache;
+    __templates_cache = null;
+  }  
+});
+
+test("UI._Templates must exist", function() {
+  ok(UI._Templates);
+});
+
+test("templates obj should retrieve theme template", function() {
+  equals(get(templates, 'a'),'aTemplate');
+  equals(get(templates, 'b'),'bTemplate');
+});
+
+test("templates obj should fall back to SC.TEMPLATES when theme template doesn't match", function() {
+  equals(get(templates, 'c'),'cTemplate');
+});
+
+test("templates obj should return null if no match was found", function() {
+  equals(get(templates, 'd'),null);
+});
