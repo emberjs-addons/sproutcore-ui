5aae2f426bc8088f5854e584169a5cce3f9a9a3b
Majd Taby <mtaby@me.com>
2011-06-22 20:45:42 -0700

    refactor navigation test

--- tests/acceptance_tests/navigation.js
+++ tests/acceptance_tests/navigation.js
@@ -14,63 +14,65 @@ var containerView;
 
 module("Navigation Acceptance test", {
   setup: function() {
+    console.group('Navigation Acceptance Test');
+
+    NavigationAcceptanceTest.navigationController = UI.NavigationController.create({
+      tree: [
+        {
+          value: 'one',
+          children: [
+            { value: "one one" },
+            { value: "one two" },
+            { value: "one three" },
+            { value: "one four" },
+            { value: "one five" }
+          ]
+        },
+        {
+          value: 'two',
+          children: [
+            { value: "two one" },
+            { value: "two two" },
+            { value: "two three" },
+            { value: "two four" },
+            { value: "two five" }
+          ]
+        },
+        {
+          value: 'three',
+          children: [
+            { value: "three one" },
+            { value: "three two" },
+            { value: "three three" },
+            { value: "three four" },
+            { value: "three five" }
+          ]
+        }
+      ]
+    });
+
+    containerView = SC.View.create({
+      elementId: 'container-view',
+      template: SC.Handlebars.compile('{{ui NavigationView elementId="navigation_view" controller="NavigationAcceptanceTest.navigationController"}}')
+    });
+
 
     SC.run(function() {
-      NavigationAcceptanceTest.navigationController = UI.NavigationController.create({
-        tree: [
-          {
-            value: 'one',
-            children: [
-              { value: "one one" },
-              { value: "one two" },
-              { value: "one three" },
-              { value: "one four" },
-              { value: "one five" }
-            ]
-          },
-          {
-            value: 'two',
-            children: [
-              { value: "two one" },
-              { value: "two two" },
-              { value: "two three" },
-              { value: "two four" },
-              { value: "two five" }
-            ]
-          },
-          {
-            value: 'three',
-            children: [
-              { value: "three one" },
-              { value: "three two" },
-              { value: "three three" },
-              { value: "three four" },
-              { value: "three five" }
-            ]
-          }
-        ]
-      });
-
-      containerView = SC.View.create({
-        template: SC.Handlebars.compile('{{ui NavigationView elementId="navigation_view" controller="NavigationAcceptanceTest.navigationController"}}')
-      });
-
-      console.group('TEST: appending containerView');
       containerView.append();
-      debugger;
     });
   },
 
   teardown: function() {
-    console.groupEnd();
     containerView.destroy();
+
+    console.groupEnd();
   }  
 });
 
 test("#navigation_view should be a valid selector in DOM", function() {
-  equals(containerView.$('#navigation_view').length,1);
+  equals($('#navigation_view').length,1);
 });
 
 test("It should create a #navigation-item for each item", function() {
-  equals(containerView.$('.ui-navigation-item').length,navigation_tree.length);
+  equals($('.ui-navigation-item').length,navigation_tree.length);
 });
