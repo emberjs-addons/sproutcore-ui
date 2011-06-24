3751e9aded151d3d8a23b983595ab061c40809e5
Majd Taby <mtaby@me.com>
2011-06-23 18:35:04 -0700

    Update unit test and start responding to clicks

--- tests/acceptance_tests/navigation.js
+++ tests/acceptance_tests/navigation.js
@@ -9,7 +9,6 @@
 var set = SC.set;
 var get = SC.get;
 
-NavigationAcceptanceTest = {};
 var containerView;
 var content = [{title:"One"},{title:"Two"},{title:"Three"}];
 
@@ -17,49 +16,19 @@ module("Navigation Acceptance test", {
   setup: function() {
     console.group('Navigation Acceptance Test');
 
-    NavigationAcceptanceTest.collectionController = SC.CollectionView.extend({
+    application = SC.Application.create();
+    application.ready();
+
+    application.collectionController = SC.CollectionView.extend({
       content: content
     }); 
 
-    NavigationAcceptanceTest.navigationController = UI.NavigationController.create({
-      tree: [
-        {
-          value: 'one',
-          children: [
-            { value: "one one" },
-            { value: "one two" },
-            { value: "one three" },
-            { value: "one four" },
-            { value: "one five" }
-          ]
-        },
-        {
-          value: 'two',
-          children: [
-            { value: "two one" },
-            { value: "two two" },
-            { value: "two three" },
-            { value: "two four" },
-            { value: "two five" }
-          ]
-        },
-        {
-          value: 'three',
-          children: [
-            { value: "three one" },
-            { value: "three two" },
-            { value: "three three" },
-            { value: "three four" },
-            { value: "three five" }
-          ]
-        }
-      ]
-    });
+    application.navigationController = UI.NavigationController.create();
 
     containerView = SC.View.create({
       elementId: 'container-view',
-      template: SC.Handlebars.compile('{{#ui NavigationView elementId="navigation_view" controller="NavigationAcceptanceTest.navigationController"}}\
-                                         {{#collection NavigationAcceptanceTest.collectionController itemClass="navigation-item"}}\
+      template: SC.Handlebars.compile('{{#ui NavigationView elementId="navigation_view" controller="application.navigationController"}}\
+                                         {{#collection application.collectionController itemClass="navigation-item"}}\
                                            <b>{{content.title}}</b>\
                                          {{/collection}}\
                                        {{/ui}}')
@@ -72,7 +41,8 @@ module("Navigation Acceptance test", {
   },
 
   teardown: function() {
-    containerView.destroy();
+    //containerView.destroy();
+    //application.destroy();
 
     console.groupEnd();
   }  
@@ -81,4 +51,10 @@ module("Navigation Acceptance test", {
 test("Using a collection as the root view in a navigation view", function() {
   equals($('#navigation_view').length,1,"#navigation_view should be a valid selector in DOM");
   equals($('.navigation-item').length,content.length,"There should be one item per content object");
+});
+
+test("Pushing views", function() {
+  item = $('.navigation-item:first');
+
+  item.trigger('click');
 });
